485da173c887bda0b8de6be773bb26a7e896c6b2
Thao Huynh <thao.huynh@outlook.com>
2023-05-14 15:25:52 -0400

    remove security

--- terraform-single-instance/main.tf
+++ terraform-single-instance/main.tf
@@ -10,9 +10,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_access_key
+  region     = var.aws_region  
 }
 
 resource "aws_instance" "app_server" {
